e95e510fd595c300d9db01862d483a9ffb4ed432
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 08:19:48 +0900

    add article model

---
+++ db/migrate/20190801134434_create_articles.rb
@@ -0,0 +1,9 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title, null: false, length: { maximum: 200 }
+      t.text :description, null: false, length { maximum: 1000 }
+      t.timestamps
+    end
+  end
+end
