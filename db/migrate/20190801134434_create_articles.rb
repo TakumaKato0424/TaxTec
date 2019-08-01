859eb625fc923bc5923a4637061244e04c8576eb
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 08:28:30 +0900

    Update 20190801134434_create_articles.rb

--- db/migrate/20190801134434_create_articles.rb
+++ db/migrate/20190801134434_create_articles.rb
@@ -2,7 +2,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
   def change
     create_table :articles do |t|
       t.string :title, null: false, length: { maximum: 200 }
-      t.text :description, null: false, length { maximum: 1000 }
+      t.text :description, null: false, length: { maximum: 1000 }
       t.timestamps
     end
   end
