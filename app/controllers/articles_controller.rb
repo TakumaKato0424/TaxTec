f228f098968365a9f73bcffa56bfc706496880bd
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 12:27:07 +0900

    Update articles_controller.rb

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,31 @@
 class ArticlesController < ApplicationController
-  def index; end;
+  def index
+    
+    @article = Article.all.order("created_at DESC")
+  end
 
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def show; end;
+  def edit; end;
+  def update; end;
+  def destroy; end;
+
+  private
+  def article_params
+    # ToDo user_id をcurrent_user.idにするようにする
+    params.require(:article).permit(:title, :description).merge(user_id: 1)
+  end
 end
