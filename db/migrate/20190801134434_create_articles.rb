e4b98af2b8c6f81a166ffdc8ec9123215b70241f
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 17:11:30 +0900

    Update 20190801134434_create_articles.rb

--- db/migrate/20190801134434_create_articles.rb
+++ db/migrate/20190801134434_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.2]
     create_table :articles do |t|
       t.string :title, null: false, length: { maximum: 200 }
       t.text :description, null: false, length: { maximum: 1000 }
+      t.string :controller_name, null: false
       t.references :user, null:false, foreign_key: true
       t.timestamps
     end
