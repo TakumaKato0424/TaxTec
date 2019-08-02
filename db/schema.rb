f21b6184c6bb2e7d8807f5b7a61310615ae8b38d
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 12:27:59 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,8 +15,10 @@ ActiveRecord::Schema.define(version: 2019_08_01_134434) do
   create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema.define(version: 2019_08_01_134434) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "articles", "users"
 end
