eb4faf58f9af9abaf578f475b50d5aa95e4fc979
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-02 12:28:09 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html 
   root 'articles#index'
-  resources :articles, only: %i[index]
-  resources :taxs, only: %i[index show]
+  resources :articles
 end
