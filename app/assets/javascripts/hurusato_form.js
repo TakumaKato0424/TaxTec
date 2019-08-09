04f1838a752904744c3ab65b89943818efe611e2
加藤拓磨 <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-08-09 18:25:15 +0900

    add hurusato simuration function

--- app/assets/javascripts/hurusato_form.js
+++ app/assets/javascripts/hurusato_form.js
@@ -31,21 +31,25 @@ var setRadioValue = function(name, value){
 };
 
 var salaryIncomeDeduction = function(income){
+  deduction = 0;
   if(0 <= income && income <= 650000){
-    return 0;
+    deduction = 0;
   } else if( 650000 < income && income <= 1625000){
-    return income - 650000;
+    deduction = income - 650000;
   } else if( 1625000 < income && income <= 1800000){
-    return (income * 0.4);
+    deduction = (income * 0.4);
   } else if( 1800000 < income && income <= 3600000){
-    return (income * 0.3) + 180000;
+    deduction = (income * 0.3) + 180000;
   } else if( 3600000 < income && income <= 6600000){
-    return (income * 0.2) + 540000;
+    deduction = (income * 0.2) + 540000;
   } else if( 6600000 < income && income <= 10000000){
-    return (income * 0.1) + 1200000;
+    deduction = (income * 0.1) + 1200000;
   } else if( 10000000< income){
-    return 2200000;
+    deduction = 2200000;
+  } else{
+    window.alert("収入金額が不正です")
   }
+  return deduction;
 };
 
 var spousalDeduction = function(spouse){
@@ -95,22 +99,62 @@ var specificDependentDeduction = function(specific_dependent){
   }
 };
 
+var getIncomeTaxRate = function(income){
+  rate = 0; 
+  if( income < 1000 ){
+    rate = 0;
+  }else if( 1000 <= income && income <= 1949000 ){
+    rate = 0.05;
+  }else if( 1950000 <= income && income <= 3299000 ){
+    rate = 0.1;
+  }else if( 3300000 <= income && income <= 6949999 ){
+    rate = 0.2;
+  }else if( 6950000 <= income && income <= 8999000 ){
+    rate = 0.23;
+  }else if( 9000000 <= income && income <= 17999000 ){
+    rate = 0.33;
+  }else if( 18000000 <= income && income <= 39999000 ){
+    rate = 0.4;
+  }else{
+    rate = 0.45;
+  }
+  return rate;
+};
+
+var roundedDown = function(numeric){
+  return Math.floor(numeric / 1000) * 1000;
+};
+
 $(document).on('turbolinks:load',function() {
-
-
+  BASIC_DEDUCTION = 330000; // 住民税基礎控除額は330000円とする
+  ADJUSTMENT = 2500; // 住民税の調整控除額は2500円とする
+  SOCIAL_INSURANCE_RATE = 0.15; // 社会保険料は15%とする
+  INHABITANT_RATE = 0.1; // 住民税率は10%とする
+  RECONSTRUCTION_RATE = 1.021; // 復興特別所得税は102.1%とする
+  
   setRadioValue('spouse', '0');
   setRadioValue('dependent', '0');
   setRadioValue('specific_dependent', '0')
-  document.getElementById('button').addEventListener('click',function(){
-    console.log(spousalDeduction(getRadioValue('spouse')));
-    console.log(dependentDeduction(getRadioValue('dependent')));
-    console.log(specificDependentDeduction(getRadioValue('specific_dependent')));
-  },false);
 
-  $('#salary_income').on('keyup', function() {
-    // console.log( $('#salary_income').val());
+  document.getElementById('button').addEventListener('click',function(){
+    // フォームから給与収入の金額を取得する
     var salary_income = Number($('#salary_income').val());
-    console.log(salary_income);
-    console.log(salaryIncomeDeduction(salary_income));
-  });
+    // ラジオボタンの情報から所得控除を求める
+    var total_deduction = salaryIncomeDeduction(salary_income) 
+                        + spousalDeduction(getRadioValue('spouse'))
+                        + dependentDeduction(getRadioValue('dependent'))
+                        + specificDependentDeduction(getRadioValue('specific_dependent'))
+                        + (salary_income * SOCIAL_INSURANCE_RATE)
+                        + BASIC_DEDUCTION;
+    // 課税所得を求める
+    var income = salary_income - total_deduction;
+    income = roundedDown(income);
+    income_rate = getIncomeTaxRate(income);
+    income_levy = (income * INHABITANT_RATE) - ADJUSTMENT;
+    limit = (income_levy * 0.2) / (0.9 - (income_rate * RECONSTRUCTION_RATE)) + 2000;
+    limit = roundedDown(limit);
+    if(limit < 0)limit = 0;
+    limit = limit.toString().replace( /\B(?=(\d{3})+(?!\d))/g, ',');
+    $('#result').text(limit);
+  },false);
 });
